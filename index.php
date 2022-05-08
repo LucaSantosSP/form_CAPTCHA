656ab71185a42b6d977fb69e52708fc0fd1cd695
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-05-08 04:25:28 -0300

    Teste de formulário

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="iNome">Nome: </label>
+        <input type="text" name="nNome" id="iNome">
+
+        <div class="g-recaptcha" data-sitekey="6LctN9EfAAAAAILh-h25sX8pbcbcN19sBIL_yKmh"></div><br>
+
+        <input type="submit" name="nEnviar" value="ENVIAR" onclick="return valida()">
+    </form>
+
+    <script type="text/javascript">
+        function valida() {
+            if (grecaptcha.getResponse() == "") {
+                alert("Você precisa marcar a caixa de validação");
+                return false;
+            }
+        }
+    </script>
+
+    <?php
+    if (isset($_POST['enviar']))
+        print_r($_POST);
+    ?>
+</body>
+</html>
